e0d4ca61d4135368b4341bf6ee0acf78df0abe1b
Guilherme <dosanjosguilherme88@gmail.com>
2025-08-30 11:15:41 -0300

    Criando o nav

---
+++ prime-burguer/src/Itens/Nav.js
@@ -0,0 +1,14 @@
+export function Nave(){
+    return(
+        <div className="my-nav">
+            <div className="img">Logo</div>
+            <div className="itens-nav">
+                <p>Carrinho</p>
+                <p>Registre-se</p>
+                <p>Contatos</p>
+            </div>
+
+        </div>
+    )
+
+}
