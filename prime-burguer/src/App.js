9ca25c2d353801a944451ddc1304b7f3686de18d
Guilherme <dosanjosguilherme88@gmail.com>
2025-08-31 13:10:20 -0300

    Estilizando meu cardapio, colocando grid e arrumando espaços

--- prime-burguer/src/App.js
+++ prime-burguer/src/App.js
@@ -12,18 +12,22 @@ function App() {
         </nav>
       </header>
       <main>
-        <section>
+        <section className='lanche'>
           <h1 className='styleh1'>Lanches</h1>
+          <div className='lanche1'>
           {itensDoCardapioLanches.map(lanche => (
             <ArticleLanches key={lanche.id} lanche={lanche} />
           ))}
+          </div>
         </section>
 
-        <section>
+        <section className='sobremessa'>
           <h1>Sobremessa</h1>
+          <div className='sobremessa1'>
           {itensDoCardapioSobremessas.map(doces => (
             <ArticleSobremessa key={doces.id} doces={doces} />
           ))}
+          </div>
         </section>
       </main>
     </div>
--- prime-burguer/src/Itens/Article.js
+++ prime-burguer/src/Itens/Article.js
@@ -1,10 +1,12 @@
 const ArticleLanches = ({lanche}) =>{
     return(
         <div className="item-lanche">
-            <h1>{lanche.item}</h1>
             <div className="under-title">
             <img src={lanche.foto}/>  
+            <div className="titulo-desc">
+            <h1>{lanche.item}</h1>
             <p>{lanche.descricao}</p>
+            </div>
             </div>
         </div>
     )
@@ -14,10 +16,13 @@ export default ArticleLanches
 export function ArticleSobremessa({doces}) {
     return (
         <div className="item-doce"> 
-            <h1>{doces.item}</h1>
+            
             <div className="under-title">
              <img src={doces.foto} />
+             <div className="titulo-desc1">
+             <h1>{doces.item}</h1>
             <p>{doces.descricao}</p>
+            </div>
             </div>
         </div>
 
