a268347aa5fbf2e65ea418ede536500bcaf7e46c
guilhermedosanjosilva77 <dosanjosguilherme88@gmail.com>
2025-10-22 22:25:14 -0300

    Criando tela inicial de administrador

--- prime-burguer/src/Pages/HomeAdmin.js
+++ prime-burguer/src/Pages/HomeAdmin.js
@@ -1,13 +1,30 @@
 import react from "react";
+import "./homeadmin.css"
 
-function HomeAdmin(){
-    return <div>
-        <nav>
-            <p>Imagem logo</p>
+function HomeAdmin() {
+  return (
+    <div className="layout">
+      <header>
+        <nav className="nave">
+            <h1>Logo</h1>
             <h2>User:</h2>
+      </nav>
+      </header>
+      <main>
+        <article>
+            <ul className="lista">
+                <l1><button>Gerenciamento de estoque</button></l1>
+                <li><button>Editar Itens do cardápio</button></li>
+                <li><button>Gerenciamento de pedidos (pedidos diários e histórico)</button></li>
+                <li><button>Criação de Itens</button></li>
+                
 
-        </nav>
+
+            </ul>
+
+        </article>
+      </main>
     </div>
-
+  );
 }
-export default HomeAdmin
+export default HomeAdmin;
